5756c5e842b79883feb0a4afe60c34536c6a7ae9
0AaalbatrosS GuY <thexcelsiorisback@gmail.com>
2021-06-08 15:33:14 +0530

    Added customizable prefix

--- main.py
+++ main.py
@@ -3,8 +3,39 @@ from discord.ext import commands
 from lib.db import db
 from decouple import config
 
+def get_prefix(bot, message):
+    prefix = db.field("SELECT Prefix FROM guilds WHERE GuildID = ?", message.guild.id)
+
+    return commands.when_mentioned_or(prefix)(bot, message)
+
 TOKEN = config('TOKEN')
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, owner_ids=['564380749873152004', '676414187131371520'])
+
+# Insert new guild into guilds prefix database indexing by GuildID
+@bot.event
+async def on_guild_join(guild):
+    db.execute("INSERT OR IGNORE INTO guilds(GuildID, Prefix) VALUES(?, ?)", guild.id, "!") # Saves guild by it's id.
+    db.commit() # save changes
+    print("new server joined") #optional
+
+
+# Removes the guild from the guilds database indexing by GuildID
+@bot.event
+async def on_guild_remove(guild):
+    db.execute("DELETE FROM guilds WHERE GuildID = ?", guild.id) # Deletes guild from the database by finding it's id.
+    db.commit() #save changes
+
+# Change prefix command
+@bot.command(name="pre", aliases=["p", "cp"])
+@commands.has_permissions(manage_guild=True)
+async def change_bot_prefix(ctx, new_prefix:str) -> str:
+    if len(new_prefix) > 7:
+        await ctx.channel.send("Server Prefix Cannot Be More Than 7 Letters Long!") # You can change the message accordingly
+    else:
+        await ctx.send(f"Prefix changed to `{new_prefix}` successfully.")
+        db.execute("UPDATE guilds SET Prefix = ? WHERE GuildID = ?", new_prefix, ctx.guild.id) # Updates server prefix indexing by GuildID
+        db.commit()
+
 
 @bot.event
 async def on_ready():
